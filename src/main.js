98092ecf9d5def616bb3827bd23dad3cfb4b146f
Anna <awdowich@gmail.com>
2022-12-21 16:52:00 +0700

    fix

--- src/main.js
+++ src/main.js
@@ -5,6 +5,6 @@ import cors from "cors";
 
 import router from "./router";
 
-const app = createApp(App).use(router).use(cors());
+const app = createApp(App).use(router).use(cors);
 
 app.mount("#app");
