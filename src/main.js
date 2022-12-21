75d4fc6a77a574c894f58f84b1e0ac2227e0ed1b
Anna <awdowich@gmail.com>
2022-12-21 16:50:20 +0700

    fix cors

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "@/assets/main.css";
+import cors from "cors";
 
 import router from "./router";
 
-const app = createApp(App).use(router);
+const app = createApp(App).use(router).use(cors());
 
 app.mount("#app");
